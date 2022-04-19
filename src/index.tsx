cc06e0d7ff6e9588f2090932f81cb180ff58b17a
gerard2perez <gerard2perez@outlook.com>
2022-04-19 02:10:21 -0500

    chore: initial

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom/client'
+import styles from './index.css'
+import styles3 from './index.sass'
+import styles2 from './index2.scss'
+
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement)
+  .render(
+    <>
+      <h1 className={styles.Style1}>
+        Hello World
+      </h1>
+      <h2 className={styles2['Style3-Csa']}>
+        Casa
+      </h2>
+      <h3 className={styles3.Style1}>
+        Casa
+      </h3>
+    </>,
+  )
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const config = /** @type { import('webpack').Configuration } */ ({
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        exclude: /(node_modules)/,
+        loader: 'ts-loader',
+        test: /\.[tj]sx?$/,
+      },
+      {
+        test: /\.(css|scss|sass)$/i,
+        use: [
+          // MiniCssExtractPlugin.loader,
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: true,
+              modules: {
+                // namedExport: true,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      // runtime: true,
+      // filename: ({ chunk }) => `${chunk.name.replace('/js/', '/css/')}.css`,
+      // chunkFilename: '[name]-[id].chunk.css'
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    compress: true,
+    port: 3000,
+  },
+})
+
+export default config
